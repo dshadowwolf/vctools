f99a92aba213a2c3e49ce79bf0219200f09ea214
Daniel R. Hazelton <dshadowwolf@gmail.com>
2019-01-14 16:02:04 -0500

    somehow forgot this one

---
+++ emulator/frontend/messages_window.c
@@ -0,0 +1,40 @@
+#include <ncurses.h>
+#include <stdio.h>
+#include <cdk_test.h>
+#include "fe_data.h"
+#include "../bcm2835_emul.h"
+#include "../vc4_emul.h"
+
+static WINDOW *mywin;
+static struct bcm2835_emul *emul_copy;
+static CDKSWINDOW *scroller;
+static CDKSCREEN *screen;
+
+void print_log(const char *fmt, ...) {
+  va_list args;
+  va_start(args, fmt);
+  char target[4096];
+  vsnprintf(target, 4096, fmt, args);
+  va_end(args);
+  
+  addCDKSwindow(scroller, target, BOTTOM);
+  
+  //  box(mywin, ACS_VLINE, ACS_HLINE);
+  wrefresh(mywin);
+}
+
+void initMessagesWindow(struct bcm2835_emul *emu) {
+  emul_copy = emu;
+  mywin = newwin(7, 133, 35, 0);
+  screen = initCDKScreen(mywin);
+  
+  box(mywin, ACS_VLINE, ACS_HLINE);
+  scroller = newCDKSwindow(screen, 36, 1, 5, 131, NULL, 512, FALSE, FALSE);
+  drawCDKSwindow (scroller, ObjOf (scroller)->box);
+  refresh();
+  wrefresh(mywin);
+}
+
+void commandMSG(int key) {
+  injectCDKSwindow(scroller, key);
+}
